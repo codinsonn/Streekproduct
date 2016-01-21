f4609d4d26904c9852a91546fb6862a45072dd01
Thorr Stevens <thorr.stevens@student.howest.be>
2016-01-21 21:35:24 +0100

    Page smooth scroll and auto detect current section

--- _js/helpers/util.js
+++ _js/helpers/util.js
@@ -28,6 +28,36 @@ export const prepend = ($parent, $element) => {
 
 };
 
+export const getTopOffset = () => {
+
+  let topOffset = (window.pageYOffset || document.documentElement.scrollTop) - (document.documentElement.clientTop || 0);
+  return topOffset;
+
+};
+
+export const animate = (elem, style, unit, from, to, time, prop) => {
+
+  if(!elem) return;
+
+  let start = new Date().getTime();
+  let timer = setInterval(() => {
+
+    let step = Math.min(1, (new Date().getTime() - start) / time);
+
+    if (prop) {
+      elem[style] = (from + step * (to - from)) + unit;
+    } else {
+      elem.style[style] = (from + step * (to - from)) + unit;
+    }
+
+    if(step === 1) clearInterval(timer);
+
+  }, 25);
+
+  elem.style[style] = from + unit;
+
+};
+
 export const removeByClassName = selector => {
 
   let $element = document.querySelector(selector);
--- _js/script.js
+++ _js/script.js
@@ -1,23 +1,81 @@
 'use strict';
 
-//import {$} from './helpers/util';
+import {animate, getTopOffset} from './helpers/util';
 
 let $burger = document.querySelector('.burger');
 let $menu = document.querySelector('.menu');
+let $links = document.querySelectorAll('.menu a');
+let $pageSections = document.querySelectorAll('.pageSection');
+
+let scrolling = false;
 
 const init = () => {
 
   console.log('[Script] Initialising...');
 
-  $burger.addEventListener('click', (e) => toggleMenu(e));
+  // -- Init Actions -------------
+  checkCurrentSection();
 
-  //$verticalNav.find('a').addEventListener('click', (event) => initScroll(event));
+  // -- Event Listeners -------------
+  $burger.addEventListener('click', (e) => toggleMenu(e));
+  window.addEventListener('scroll', (e) => checkCurrentSection(e));
+  for(let i = 0; i < $links.length; i++){
+    $links[i].addEventListener('click', (e) => checkAnchorAndScroll(e));
+  }
 
 };
 
-const toggleMenu = (e) => {
+const checkCurrentSection = () => {
 
-  console.log(e);
+  if(scrolling === false){
+    for(let i = 0; i < $pageSections.length; i++){
+
+      let checkOffset = $pageSections[i].offsetTop + $pageSections[i].clientHeight - 140;
+      if(getTopOffset() >= $pageSections[i].offsetTop && getTopOffset() <= checkOffset){
+        setActiveLinks(`#${$pageSections[i].getAttribute('id')}`);
+      }
+
+    }
+  }
+
+};
+
+const checkAnchorAndScroll = (e) => {
+
+  let $link = e.currentTarget;
+  let href = $link.getAttribute('href');
+
+  if(href.substr(0, 1) === '#'){
+
+    e.preventDefault();
+
+    let target = document.querySelector(`#${href.substr(1, href.length)}`);
+
+    setActiveLinks(href);
+
+    scrolling = true;
+    animate(document.documentElement, 'scrollTop', '', getTopOffset(), target.offsetTop - 8, 600, true);
+    setTimeout(() => {
+      scrolling = false;
+    }, 580);
+
+  }
+
+};
+
+const setActiveLinks = (href) => {
+
+  for(let i = 0; i < $links.length; i++){
+    if($links[i].getAttribute('href') === href){
+      $links[i].className = 'active';
+    }else{
+      $links[i].className = '';
+    }
+  }
+
+};
+
+const toggleMenu = () => {
 
   if($burger.className === 'burger closed'){
     $burger.className = 'burger open';
