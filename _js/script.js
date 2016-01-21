fcb782162c81d96abbd86afa265ec46d3efaee88
Thorr Stevens <thorr.stevens@student.howest.be>
2016-01-21 18:38:20 +0100

    Basic Burger Nav Toggle Animation

--- _js/script.js
+++ _js/script.js
@@ -1,8 +1,31 @@
 'use strict';
 
+//import {$} from './helpers/util';
+
+let $burger = document.querySelector('.burger');
+let $menu = document.querySelector('.menu');
+
 const init = () => {
 
   console.log('[Script] Initialising...');
+
+  $burger.addEventListener('click', (e) => toggleMenu(e));
+
+  //$verticalNav.find('a').addEventListener('click', (event) => initScroll(event));
+
+};
+
+const toggleMenu = (e) => {
+
+  console.log(e);
+
+  if($burger.className === 'burger closed'){
+    $burger.className = 'burger open';
+    $menu.className = 'menu open';
+  }else{
+    $burger.className = 'burger closed';
+    $menu.className = 'menu closed';
+  }
 
 };
 
--- js/script.js
+++ js/script.js
@@ -44,19 +44,21 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	eval("__webpack_require__(1);\nmodule.exports = __webpack_require__(2);\n\n\n/*****************\n ** WEBPACK FOOTER\n ** multi main\n ** module id = 0\n ** module chunks = 0\n **/\n//# sourceURL=webpack:///multi_main?");
+	eval("__webpack_require__(1);\nmodule.exports = __webpack_require__(4);\n\n\n/*****************\n ** WEBPACK FOOTER\n ** multi main\n ** module id = 0\n ** module chunks = 0\n **/\n//# sourceURL=webpack:///multi_main?");
 
 /***/ },
 /* 1 */
 /***/ function(module, exports) {
 
-	eval("'use strict';\n\nvar init = function init() {\n\n  console.log('[Script] Initialising...');\n};\n\ninit();\n\n/*****************\n ** WEBPACK FOOTER\n ** ./_js/script.js\n ** module id = 1\n ** module chunks = 0\n **/\n//# sourceURL=webpack:///./_js/script.js?");
+	eval("'use strict';\n\n//import {$} from './helpers/util';\n\nvar $burger = document.querySelector('.burger');\nvar $menu = document.querySelector('.menu');\n\nvar init = function init() {\n\n  console.log('[Script] Initialising...');\n\n  $burger.addEventListener('click', function (e) {\n    return toggleMenu(e);\n  });\n\n  //$verticalNav.find('a').addEventListener('click', (event) => initScroll(event));\n};\n\nvar toggleMenu = function toggleMenu(e) {\n\n  console.log(e);\n\n  if ($burger.className === 'burger closed') {\n    $burger.className = 'burger open';\n    $menu.className = 'menu open';\n  } else {\n    $burger.className = 'burger closed';\n    $menu.className = 'menu closed';\n  }\n};\n\ninit();\n\n/*****************\n ** WEBPACK FOOTER\n ** ./_js/script.js\n ** module id = 1\n ** module chunks = 0\n **/\n//# sourceURL=webpack:///./_js/script.js?");
 
 /***/ },
-/* 2 */
+/* 2 */,
+/* 3 */,
+/* 4 */
 /***/ function(module, exports) {
 
-	eval("// removed by extract-text-webpack-plugin\n\n/*****************\n ** WEBPACK FOOTER\n ** ./_scss/style.scss\n ** module id = 2\n ** module chunks = 0\n **/\n//# sourceURL=webpack:///./_scss/style.scss?");
+	eval("// removed by extract-text-webpack-plugin\n\n/*****************\n ** WEBPACK FOOTER\n ** ./_scss/style.scss\n ** module id = 4\n ** module chunks = 0\n **/\n//# sourceURL=webpack:///./_scss/style.scss?");
 
 /***/ }
 /******/ ]);
