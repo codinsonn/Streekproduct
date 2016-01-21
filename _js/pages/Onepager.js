da0f05064cc9bd2eaa30c50013c74a7a5c0049ea
Thorr Stevens <thorr.stevens@student.howest.be>
2016-01-21 22:04:11 +0100

    Split pages into classes

---
+++ _js/pages/Onepager.js
@@ -0,0 +1,81 @@
+'use strict';
+
+import {animate, getTopOffset} from '../helpers/util';
+
+export default class Onepager{
+
+  constructor(){
+
+    // -- Class Variables -------------
+    this.scrolling = false;
+
+    // -- Element Variables -----------
+    this.$links = document.querySelectorAll('.menu a');
+    this.$pageSections = document.querySelectorAll('.pageSection');
+
+    // -- Event Listeners -------------
+    window.addEventListener('scroll', (e) => this.checkCurrentSection(e));
+    for(let i = 0; i < this.$links.length; i++){
+      this.$links[i].addEventListener('click', (e) => this.checkAnchorAndScroll(e));
+    }
+
+  }
+
+  init(){
+
+    // Updates 'a.active' when refreshing once scrolling has taken place.
+    this.checkCurrentSection();
+
+  }
+
+  checkCurrentSection(){
+
+    if(this.scrolling === false){
+      for(let i = 0; i < this.$pageSections.length; i++){
+
+        let checkOffset = this.$pageSections[i].offsetTop + this.$pageSections[i].clientHeight - 140;
+        if(getTopOffset() >= this.$pageSections[i].offsetTop && getTopOffset() <= checkOffset){
+          this.setActiveLinks(`#${this.$pageSections[i].getAttribute('id')}`);
+        }
+
+      }
+    }
+
+  }
+
+  checkAnchorAndScroll(e){
+
+    let $link = e.currentTarget;
+    let href = $link.getAttribute('href');
+
+    if(href.substr(0, 1) === '#'){
+
+      e.preventDefault();
+
+      let target = document.querySelector(`#${href.substr(1, href.length)}`);
+
+      this.setActiveLinks(href);
+
+      this.scrolling = true;
+      animate(document.documentElement, 'scrollTop', '', getTopOffset(), target.offsetTop - 8, 600, true);
+      setTimeout(() => {
+        this.scrolling = false;
+      }, 580);
+
+    }
+
+  }
+
+  setActiveLinks(href){
+
+    for(let i = 0; i < this.$links.length; i++){
+      if(this.$links[i].getAttribute('href') === href){
+        this.$links[i].className = 'active';
+      }else{
+        this.$links[i].className = '';
+      }
+    }
+
+  }
+
+}
--- _js/script.js
+++ _js/script.js
@@ -1,76 +1,33 @@
 'use strict';
 
-import {animate, getTopOffset} from './helpers/util';
+import Onepager from './pages/Onepager';
+
+let onepager;
 
 let $burger = document.querySelector('.burger');
 let $menu = document.querySelector('.menu');
-let $links = document.querySelectorAll('.menu a');
-let $pageSections = document.querySelectorAll('.pageSection');
-
-let scrolling = false;
 
 const init = () => {
 
-  console.log('[Script] Initialising...');
-
-  // -- Init Actions -------------
-  checkCurrentSection();
-
-  // -- Event Listeners -------------
   $burger.addEventListener('click', (e) => toggleMenu(e));
-  window.addEventListener('scroll', (e) => checkCurrentSection(e));
-  for(let i = 0; i < $links.length; i++){
-    $links[i].addEventListener('click', (e) => checkAnchorAndScroll(e));
-  }
+
+  initCurrentPage();
 
 };
 
-const checkCurrentSection = () => {
+const initCurrentPage = () => {
 
-  if(scrolling === false){
-    for(let i = 0; i < $pageSections.length; i++){
+  switch(document.querySelector('.active').getAttribute('href')){
 
-      let checkOffset = $pageSections[i].offsetTop + $pageSections[i].clientHeight - 140;
-      if(getTopOffset() >= $pageSections[i].offsetTop && getTopOffset() <= checkOffset){
-        setActiveLinks(`#${$pageSections[i].getAttribute('id')}`);
-      }
+  case '#intro':
+  case '#cadeaus':
+  case '#win':
+  default:
+    console.log('[Script] Intialising Onepager...');
+    onepager = new Onepager();
+    onepager.init();
+    break;
 
-    }
-  }
-
-};
-
-const checkAnchorAndScroll = (e) => {
-
-  let $link = e.currentTarget;
-  let href = $link.getAttribute('href');
-
-  if(href.substr(0, 1) === '#'){
-
-    e.preventDefault();
-
-    let target = document.querySelector(`#${href.substr(1, href.length)}`);
-
-    setActiveLinks(href);
-
-    scrolling = true;
-    animate(document.documentElement, 'scrollTop', '', getTopOffset(), target.offsetTop - 8, 600, true);
-    setTimeout(() => {
-      scrolling = false;
-    }, 580);
-
-  }
-
-};
-
-const setActiveLinks = (href) => {
-
-  for(let i = 0; i < $links.length; i++){
-    if($links[i].getAttribute('href') === href){
-      $links[i].className = 'active';
-    }else{
-      $links[i].className = '';
-    }
   }
 
 };
